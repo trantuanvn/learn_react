ab7c99c2f58e0e1471fa806ea4f0e8f54cae567f
Robin Tran <robin@splashinteractive.com.vn>
2019-07-16 15:22:02 +0700

    Updates

---
+++ precache-manifest.6d01c0333c9f25440b2edf153f0ec22f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trantuanvn/learn_react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fa1d3ed9deb8bc278557",
+    "url": "/trantuanvn/learn_react/static/js/runtime~main.fa1d3ed9.js"
+  },
+  {
+    "revision": "899eb689dbae6ee807a5",
+    "url": "/trantuanvn/learn_react/static/js/main.899eb689.chunk.js"
+  },
+  {
+    "revision": "66e0f3aa5c7ad1394bfd",
+    "url": "/trantuanvn/learn_react/static/js/1.66e0f3aa.chunk.js"
+  },
+  {
+    "revision": "899eb689dbae6ee807a5",
+    "url": "/trantuanvn/learn_react/static/css/main.86474c6e.chunk.css"
+  },
+  {
+    "revision": "66e0f3aa5c7ad1394bfd",
+    "url": "/trantuanvn/learn_react/static/css/1.bbcc09bd.chunk.css"
+  },
+  {
+    "revision": "dc2eb96a35b0c670e49e466154d10ab6",
+    "url": "/trantuanvn/learn_react/index.html"
+  }
+];
